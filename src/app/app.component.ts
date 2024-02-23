1b701c2596f5990963183cdf00173903867ed54e
Lucas Mega <lucas.mega07@gmail.com>
2024-02-22 22:24:28 -0300

    FEAT(app.component.*) Implementando itens de menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
+export interface Item {
+  name: string,
+  icon: string,
+  path: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent  implements OnInit {
+export class AppComponent implements OnInit {
+
+  items: Item[] = [
+    {
+      name: 'Início',
+      icon: 'home-outline',
+      path: ''
+    },
+    {
+      name: 'Buscar barbearia',
+      icon: 'cut-outline',
+      path: ''
+    },
+    {
+      name: 'Agendamentos',
+      icon: 'calendar-number-outline',
+      path: ''
+    },
+    {
+      name: 'Planos',
+      icon: 'arrow-up-circle-outline',
+      path: ''
+    },
+    {
+      name: 'Sair',
+      icon: 'exit-outline',
+      path: ''
+    },
+  ]
 
   constructor(public authService: AuthService) { }
 
